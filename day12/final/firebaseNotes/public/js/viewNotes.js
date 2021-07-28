1c3e713cd956516701b7e94fe9c2ed8962acd8a2
Rachelle Hu <rachelle.hu123@gmail.com>
2021-07-28 18:22:12 -0400

    Update viewNotes.js

--- day12/final/firebaseNotes/public/js/viewNotes.js
+++ day12/final/firebaseNotes/public/js/viewNotes.js
@@ -1,51 +1,98 @@
-window.onload = (event) => {
-  // Use this to retain user state between html pages.
-  firebase.auth().onAuthStateChanged(function(user) {
+let googleUser;
+
+window.onload = event => {
+  // Firebase authentication goes here.
+  firebase.auth().onAuthStateChanged(user => {
     if (user) {
-      console.log('Logged in as: ' + user.displayName);
-      const googleUserId = user.uid;
-      getNotes(googleUserId);
+      // Console log the user to confirm they are logged in
+      console.log("Logged in as: " + user.displayName);
+      googleUser = user;
+      getNotes(googleUser.uid);
     } else {
       // If not logged in, navigate back to login page.
-      window.location = 'index.html'; 
-    };
+      window.location = "index.html";
+    }
   });
 };
 
-const getNotes = (userId) => {
+const getNotes = userId => {
   const notesRef = firebase.database().ref(`users/${userId}`);
-  notesRef.on('value', (snapshot) => {
+
+  notesRef.on("value", snapshot => {
     const data = snapshot.val();
     renderDataAsHtml(data);
   });
 };
 
+//Given a list of notes, render them in HTML
 const renderDataAsHtml = (data) => {
   let cards = ``;
-  for(const noteItem in data) {
+  for (const noteItem in data) {
     const note = data[noteItem];
-    // For each note create an HTML card
-    cards += createCard(note)
-  };
-  // Inject our string of HTML into our viewNotes.html page
-  document.querySelector('#app').innerHTML = cards;
+    cards += createCard(note);
+  }
+  document.querySelector("#app").innerHTML = cards;
 };
 
-const createCard = (note) => {
-  let innerHTML = "";
-  innerHTML += `<div class="column is-one-quarter">`
-  innerHTML += `<div class="card">`
-  innerHTML += `<header class="card-header">`
-  innerHTML += `<p class="card-header-title">`
-  innerHTML += `${note.title}`
-  innerHTML += `</p>`
-  innerHTML += `</header>`
-  innerHTML += `<div class="card-content">`
-  innerHTML += `<div class="content">`
-  innerHTML += `${note.text}`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  innerHTML += `</div>`
-  return innerHTML;
+// Return a note object converted into an HTML card
+const createCard = note => {
+  let colors = ["PaleVioletRed", "Salmon", "MediumAquaMarine", "PowderBlue", "Plum", "LightCoral", "PaleTurquoise"]
+  
+  let random_color = colors[`${Math.floor(Math.random() * 7)}`]
+  
+  let classLabels = ""
+  let joinedLabels = "";
+  if (note.labels) {
+    joinedLabels = note.labels.join(", ");
+    classLabels = note.labels.join(" ");
+  }
+  const cardId = note.title + note.text;
+  
+  let card = ``;
+  card += `<div class="column is-one-quarter">
+           <div class="card ${classLabels}" id="${cardId}" style="background: ${random_color}">
+             <header class="card-header">
+               <p class="card-header-title">${note.title} 
+               <!-- <button class="button">Delete note :(</button> -->
+               </p>
+             </header>
+             <div class="card-content">
+               <div class="content">${note.text}</div>`
+               
+   if (note.image) {
+     card += `<img src="${note.image}"/>`
+   }
+               
+   card += `<p><strong>Labels:</strong></p>
+               ${joinedLabels}
+             </div>
+             
+             <div class="card-footer-item">
+               <div class="footer-item">By <strong>${googleUser.email}</strong> on ${formatDate(note.created)}</div>
+            </div>
+               
+           </div>
+         </div> `;
+  return card
 };
+
+const formatDate = (dateString) => {
+  const options = { year: "numeric", month: "long", day: "numeric" }
+  return new Date(dateString).toLocaleDateString(undefined, options)
+}
+
+function filterByLabel() {
+  const findLabel = document.querySelector("#findLabel");
+  const findLabelVal = "." + findLabel.value;
+  const notes = document.querySelectorAll(findLabelVal);
+  
+  let cards = ``;
+  for (var i=0; i<notes.length; i++) {
+    cards += '<div class="column is-one-quarter">';
+    cards += notes[i].outerHTML;
+    cards += '</div>';
+  }
+  
+  document.querySelector("#app").innerHTML = cards;
+  findLabel.value = "";
+}
